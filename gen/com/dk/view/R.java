3705f6640883877df3a1d7a76c0331555cfbeef3
Peter Ding <peterding@augmentum.com.cn>
2014-07-30 14:22:14 +0800

    remove bin

--- gen/com/dk/view/R.java
+++ gen/com/dk/view/R.java
@@ -29,12 +29,11 @@ public final class R {
         public static final int tansparent=0x7f020005;
     }
     public static final class id {
-        public static final int action_scale=0x7f080004;
-        public static final int action_translate=0x7f080005;
-        public static final int action_twister=0x7f080003;
-        public static final int image=0x7f080001;
+        public static final int action_scale=0x7f080003;
+        public static final int action_translate=0x7f080004;
+        public static final int action_twister=0x7f080002;
         public static final int list=0x7f080000;
-        public static final int text=0x7f080002;
+        public static final int text=0x7f080001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
